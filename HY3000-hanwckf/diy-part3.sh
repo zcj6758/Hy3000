7f0dcb3a63c01b647717230c97261e8f1776cd06
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-07-09 15:11:56 +0800

    Update diy-part3.sh

--- HY3000-hanwckf/diy-part3.sh
+++ HY3000-hanwckf/diy-part3.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 cp -r openwrt-passwall/luci-app-passwall package/
