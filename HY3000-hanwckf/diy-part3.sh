775aa2ce74f4bbb518c8b2f14abc0b6f6fbfcb4d
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-07-09 15:53:23 +0800

    Update diy-part3.sh

--- HY3000-hanwckf/diy-part3.sh
+++ HY3000-hanwckf/diy-part3.sh
@@ -20,8 +20,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git
 cp -r openwrt-passwall/luci-app-passwall package/
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
 cp -r openwrt-passwall-packages package/
-git clone https://github.com/Mattaclp/luci-app-openclash.git
-cp -r luci-app-openclash package/
+#git clone https://github.com/Mattaclp/luci-app-openclash.git
+#cp -r luci-app-openclash package/
 
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-openclash
@@ -42,6 +42,24 @@ rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/v2ray-plugin
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/xray-plugin
+
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../
+  cd .. && rm -rf $repodir
+}
+
+git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
+cp -rf luci-app-openclash package
+
+mkdir bin
+mkdir bin/packages
+cp -r package/luci-app-openclash bin/packages/
+zip -r luci-app-openclash.zip bin/packages/luci-app-openclash
+cp -r luci-app-openclash.zip bin/packages/luci-app-openclash.zip
 
 ##-----------------Add OpenClash dev core------------------
 #curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
